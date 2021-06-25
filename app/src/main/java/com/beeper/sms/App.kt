1bbdabb558a93bf9768f9bb95f33c17b61a91633
Alex Baker <alex@tasks.org>
2021-06-25 16:59:25 -0500

    Remove WorkManager

--- app/src/main/java/com/beeper/sms/App.kt
+++ app/src/main/java/com/beeper/sms/App.kt
@@ -1,18 +1,10 @@
 package com.beeper.sms
 
 import android.app.Application
-import android.util.Log
-import androidx.hilt.work.HiltWorkerFactory
-import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
 @HiltAndroidApp
-class App : Application(), Configuration.Provider {
-    @Inject lateinit var workerFactory: HiltWorkerFactory
+class App : Application() {
 
-    override fun getWorkManagerConfiguration(): Configuration = Configuration.Builder()
-        .setWorkerFactory(workerFactory)
-        .setMinimumLoggingLevel(if (BuildConfig.DEBUG) Log.DEBUG else Log.INFO)
-        .build()
 }
