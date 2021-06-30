331b413304fcafa363a0016a7a6acbcebf6acc7a
Alex Baker <alex@tasks.org>
2021-06-30 09:44:02 -0500

    Add network constraint to SendSms work

--- sms/src/main/java/com/beeper/sms/work/WorkManager.kt
+++ sms/src/main/java/com/beeper/sms/work/WorkManager.kt
@@ -2,8 +2,7 @@ package com.beeper.sms.work
 
 import android.content.Context
 import android.net.Uri
-import androidx.work.Data
-import androidx.work.OneTimeWorkRequest
+import androidx.work.*
 import androidx.work.WorkManager
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
@@ -15,6 +14,9 @@ class WorkManager @Inject constructor(@ApplicationContext context: Context) {
         OneTimeWorkRequest
             .Builder(SendSms::class.java)
             .setInputData(Data.Builder().putString(SendSms.URI, uri.toString()).build())
+            .setConstraints(
+                Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
+            )
             .build()
             .apply { workManager.enqueue(this) }
 }
