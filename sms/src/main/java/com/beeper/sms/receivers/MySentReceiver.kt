7e054852cfceffa33d961bd57eb38f21aea9ff06
Alex Baker <alex@tasks.org>
2021-07-01 11:42:03 -0500

    Must override SentReceiver
    Otherwise message type not updated properly

--- sms/src/main/java/com/beeper/sms/receivers/MySentReceiver.kt
+++ sms/src/main/java/com/beeper/sms/receivers/MySentReceiver.kt
@@ -1,39 +1,50 @@
 package com.beeper.sms.receivers
 
-import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import androidx.core.net.toUri
 import com.beeper.sms.Bridge
-import com.beeper.sms.commands.Command
 import com.beeper.sms.SmsProvider
+import com.beeper.sms.commands.Command
 import com.beeper.sms.commands.incoming.SendMessage
+import com.beeper.sms.extensions.printExtras
 import com.klinker.android.send_message.SentReceiver
 import com.klinker.android.send_message.Transaction.SENT_SMS_BUNDLE
-import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.android.EntryPointAccessors
+import dagger.hilt.components.SingletonComponent
 
-@AndroidEntryPoint
-class MySentReceiver : BroadcastReceiver() {
-    @Inject lateinit var bridge: Bridge
-    @Inject lateinit var smsProvider: SmsProvider
+@EntryPoint
+@InstallIn(SingletonComponent::class)
+interface MySentReceiverEntryPoint {
+    val bridge: Bridge
+}
 
-    override fun onReceive(context: Context?, intent: Intent?) = object : SentReceiver() {
-        override fun onMessageStatusUpdated(context: Context?, intent: Intent?, resultCode: Int) {
-            val command = intent?.getParcelableExtra(SENT_SMS_BUNDLE) as? Command ?: return
-            val message =
-                intent
-                    .getStringExtra("message_uri")
-                    ?.toUri()
-                    ?.let { smsProvider.getMessage(it) }
-                    ?: return
-            bridge.send(
+class MySentReceiver : SentReceiver() {
+    override fun onMessageStatusUpdated(context: Context, intent: Intent?, resultCode: Int) {
+        Log.d(TAG, intent.printExtras())
+        val uri = intent?.getStringExtra("message_uri")?.toUri()
+        val commandId = (intent?.getParcelableExtra(SENT_SMS_BUNDLE) as? Command)?.id
+        val message = uri?.let { SmsProvider(context).getMessage(it) }
+        if (uri == null || commandId == null || message == null) {
+            Log.e(TAG, "Unable to respond (command=$commandId uri=$uri)")
+            return
+        }
+        EntryPointAccessors
+            .fromApplication(context, MySentReceiverEntryPoint::class.java)
+            .bridge
+            .send(
                 Command(
                     "response",
                     SendMessage.Response(message.guid, message.timestamp),
-                    command.id,
+                    commandId,
                 )
             )
-        }
-    }.onReceive(context, intent)
+    }
+
+    companion object {
+        private const val TAG = "MySentReceiver"
+    }
 }
