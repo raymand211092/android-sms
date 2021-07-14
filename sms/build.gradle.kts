317945e4e830e6954bb1483eaf096c32cedaf5bc
Alex Baker <alex@tasks.org>
2021-07-13 23:43:28 -0500

    Add publish task

--- sms/build.gradle.kts
+++ sms/build.gradle.kts
@@ -3,6 +3,13 @@ plugins {
     kotlin("android")
     kotlin("kapt")
     id("kotlin-parcelize")
+    id("maven-publish")
+}
+
+object Version {
+    const val major = 0
+    const val minor = 1
+    const val patch = 0
 }
 
 android {
@@ -12,8 +19,6 @@ android {
     defaultConfig {
         minSdk = 23
         targetSdk = 30
-//        versionCode = 1
-//        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -37,8 +42,48 @@ android {
     }
 }
 
-dependencies {
+afterEvaluate {
+    publishing {
+        repositories {
+            maven {
+                url = uri("https://gitlab.com/api/v4/projects/27629761/packages/maven")
+                credentials(HttpHeaderCredentials::class) {
+                    name = "Deploy-Token"
+                    value = System.getenv("CI_TOKEN")
+                }
+                authentication {
+                    create<HttpHeaderAuthentication>("header")
+                }
+            }
+        }
+        publications {
+            create<MavenPublication>("gitlab") {
+                from(components["release"])
+                groupId = "com.beeper"
+                artifactId = "sms"
+                version = "${Version.major}.${Version.minor}.${Version.patch}"
+                pom {
+                    name.set("Beeper SMS Bridge")
+                    description.set("Matrix-SMS bridge for Android")
+                    url.set("https://beeper.com")
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:git://gitlab.com/beeper/android-sms.git")
+                        developerConnection.set("scm:git:ssh://gitlab.com/beeper/android-sms.git")
+                        url.set("https://gitlab.com/beeper/android-sms.git")
+                    }
+                }
+            }
+        }
+    }
+}
 
+dependencies {
     implementation("androidx.work:work-runtime-ktx:${Versions.work}")
     implementation("com.klinkerapps:android-smsmms:5.2.6")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0")
