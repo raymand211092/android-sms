a87c9a850b8b2a11f662e36844158e84a1a62593
Alex Baker <alex@tasks.org>
2021-07-29 17:28:05 -0500

    Use openContactPhotoInputStream

--- sms/src/main/java/com/beeper/sms/provider/ContactProvider.kt
+++ sms/src/main/java/com/beeper/sms/provider/ContactProvider.kt
@@ -1,21 +1,19 @@
 package com.beeper.sms.provider
 
 import android.Manifest
+import android.content.ContentUris
 import android.content.Context
 import android.net.Uri
 import android.os.Build
 import android.provider.ContactsContract
 import android.provider.ContactsContract.CommonDataKinds.Phone
 import android.provider.ContactsContract.CommonDataKinds.StructuredName
+import android.provider.ContactsContract.Contacts
 import android.provider.ContactsContract.PhoneLookup
+import android.util.Base64
 import com.beeper.sms.extensions.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import android.graphics.Bitmap
-import android.graphics.ImageDecoder
-import android.provider.MediaStore
-import android.util.Base64
-import java.io.ByteArrayOutputStream
 
 class ContactProvider constructor(private val context: Context) {
     private val cr = context.contentResolver
@@ -38,19 +36,12 @@ class ContactProvider constructor(private val context: Context) {
     suspend fun searchContacts(text: String): List<ContactRow> =
         withContext(Dispatchers.IO) {
             val uri = Uri.withAppendedPath(
-                if (text.isBlank()) {
-                    ContactsContract.Contacts.CONTENT_URI
-                } else {
-                    ContactsContract.Contacts.CONTENT_FILTER_URI
-                },
+                if (text.isBlank()) Contacts.CONTENT_URI else Contacts.CONTENT_FILTER_URI,
                 Uri.encode(text)
             )
             cr
-                .flatMap(
-                    uri,
-                    where = "${ContactsContract.Contacts.HAS_PHONE_NUMBER} = 1",
-                ) { c ->
-                    val id = c.getLong(ContactsContract.Contacts._ID)
+                .flatMap(uri, where = "${Contacts.HAS_PHONE_NUMBER} = 1") { c ->
+                    val id = c.getLong(Contacts._ID)
                     val contact = getContact(id) ?: return@flatMap emptyList<ContactRow>()
                     cr.map(
                         ContactsContract.Data.CONTENT_URI,
@@ -78,26 +69,14 @@ class ContactProvider constructor(private val context: Context) {
             ContactsContract.Data.CONTENT_URI,
             "${ContactsContract.Data.CONTACT_ID} = $id AND ${ContactsContract.Data.MIMETYPE} = '${StructuredName.CONTENT_ITEM_TYPE}'"
         ) { contact ->
-            val contactPhotoUri = contact.getString(StructuredName.PHOTO_URI)
-            val contactPhoto = if(contactPhotoUri !== null) {
-                val bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                    ImageDecoder.decodeBitmap(ImageDecoder.createSource(cr, Uri.parse(contactPhotoUri)))
-                } else {
-                    MediaStore.Images.Media.getBitmap(cr, Uri.parse(contactPhotoUri))
-                }
-                val byteArrayOutputStream = ByteArrayOutputStream()
-                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
-                    val byteArray: ByteArray = byteArrayOutputStream.toByteArray()
-                    Base64.encodeToString(byteArray,Base64.DEFAULT)
-                } else {
-                    null
-                }
+            val contactUri = ContentUris.withAppendedId(Contacts.CONTENT_URI, id)
             val givenName = contact.getString(StructuredName.GIVEN_NAME)
             val familyName = contact.getString(StructuredName.FAMILY_NAME)
             ContactRow(
                 first_name = givenName,
                 last_name = familyName,
-                avatar = contactPhoto,
+                avatar = Contacts.openContactPhotoInputStream(cr, contactUri, true)
+                    ?.let { Base64.encodeToString(it.readBytes(), Base64.DEFAULT) },
                 nickname = contact.getString(StructuredName.DISPLAY_NAME)
                     ?: "$givenName $familyName".trim().takeIf { it.isNotBlank() }
             )
