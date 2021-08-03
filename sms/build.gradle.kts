c393ddb9df0f0da91f301ada27db28ae99397008
Alex Baker <alex@tasks.org>
2021-08-03 18:04:37 -0500

    Only start bridge if app is default sms handler

--- sms/build.gradle.kts
+++ sms/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 object Version {
     const val major = 0
     const val minor = 1
-    const val patch = 12
+    const val patch = 13
 }
 
 android {
--- sms/src/main/java/com/beeper/sms/receivers/DefaultSmsAppReceiver.kt
+++ sms/src/main/java/com/beeper/sms/receivers/DefaultSmsAppReceiver.kt
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.provider.Telephony
+import android.provider.Telephony.Sms.Intents.EXTRA_IS_DEFAULT_SMS_APP
 import android.util.Log
 import com.beeper.sms.Bridge
 
@@ -12,7 +13,9 @@ class DefaultSmsAppReceiver : BroadcastReceiver() {
         when (intent?.action) {
             Telephony.Sms.Intents.ACTION_EXTERNAL_PROVIDER_CHANGE,
             Telephony.Sms.Intents.ACTION_DEFAULT_SMS_PACKAGE_CHANGED ->
-                Bridge.INSTANCE.start(context)
+                if (intent.getBooleanExtra(EXTRA_IS_DEFAULT_SMS_APP, false)) {
+                    Bridge.INSTANCE.start(context)
+                }
             else ->
                 Log.d(TAG, "Ignoring broadcast: ${intent?.action}")
         }
