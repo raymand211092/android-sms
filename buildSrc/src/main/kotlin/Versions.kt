82260a6e6b2b021664774c377b9a1eeb930edb96
Alex Baker <alex@tasks.org>
2021-10-08 14:49:58 -0500

    Compose 1.0.3, Kotlin 1.5.30

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,5 +1,5 @@
 object Versions {
-    const val compose = "1.0.1"
-    const val kotlin = "1.5.21"
+    const val compose = "1.0.3"
+    const val kotlin = "1.5.30"
     const val work = "2.5.0"
 }
