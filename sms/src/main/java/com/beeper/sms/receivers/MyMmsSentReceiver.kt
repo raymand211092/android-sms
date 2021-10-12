a2fbdb4ee1eaf15a3038d0d827f9e959ff856175
Alex Baker <alex@tasks.org>
2021-10-12 17:58:31 -0500

    Ack messages sent by carrier app

--- sms/src/main/java/com/beeper/sms/receivers/MyMmsSentReceiver.kt
+++ sms/src/main/java/com/beeper/sms/receivers/MyMmsSentReceiver.kt
@@ -13,6 +13,8 @@ import com.beeper.sms.commands.incoming.SendMedia
 import com.beeper.sms.extensions.printExtras
 import com.beeper.sms.provider.MmsProvider
 import com.klinker.android.send_message.Transaction.SENT_MMS_BUNDLE
+import java.lang.System.currentTimeMillis
+import java.util.*
 
 class MyMmsSentReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent?) {
@@ -21,22 +23,36 @@ class MyMmsSentReceiver : BroadcastReceiver() {
         val commandId =
             (intent?.getParcelableExtra(SENT_MMS_BUNDLE) as? Bundle)?.getInt(EXTRA_COMMAND_ID)
         val message = uri?.let { MmsProvider(context).getMessage(it) }
-        when {
-            uri == null -> Log.e(TAG, "Missing uri")
-            message != null ->
-                Bridge.INSTANCE.send(
-                    Command(
-                        "response",
-                        SendMedia.Response(message.guid, message.timestamp),
-                        commandId,
-                    )
-                )
-            else -> Log.e(TAG, "Unable to respond (command=$commandId uri=$uri)")
+        val failureCause =
+            intent?.getIntExtra(EXTRA_LAST_CONNECTION_FAILURE_CAUSE_CODE, 0)
+        val handledByCarrierApp =
+            intent?.getBooleanExtra(EXTRA_HANDLED_BY_CARRIER_APP, false) ?: false
+        val (guid, timestamp) = when {
+            commandId == null -> {
+                Log.e(TAG, "missing command")
+                return
+            }
+            message != null -> Pair(message.guid, message.timestamp)
+            failureCause == 0 && handledByCarrierApp -> {
+                Log.w(TAG, "$commandId handled by carrier app")
+                Pair(UUID.randomUUID().toString(), currentTimeMillis())
+            }
+            else -> {
+                Log.e(TAG, "message not found (commandId=$commandId, uri=$uri, failureCause=$failureCause, handledByCarrierApp=$handledByCarrierApp)")
+                return
+            }
         }
+        Bridge.INSTANCE.send(
+            Command("response", SendMedia.Response(guid, timestamp), commandId)
+        )
     }
 
     companion object {
         private const val TAG = "MyMmsSentReceiver"
         private const val EXTRA_URI = "uri"
+        private const val EXTRA_LAST_CONNECTION_FAILURE_CAUSE_CODE =
+            "android.telephony.extra.LAST_CONNECTION_FAILURE_CAUSE_CODE"
+        private const val EXTRA_HANDLED_BY_CARRIER_APP =
+            "android.telephony.extra.HANDLED_BY_CARRIER_APP"
     }
 }
