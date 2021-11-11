ddf6bb6ff2bc101bb331f8f01e5d63fe6b4e4b28
Alex Baker <alex@tasks.org>
2021-11-11 17:26:37 -0600

    Error response if outgoing message missing from db

--- sms/build.gradle.kts
+++ sms/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 object Version {
     const val major = 0
     const val minor = 1
-    const val patch = 41
+    const val patch = 42
 }
 
 android {
--- sms/src/main/java/com/beeper/sms/receivers/MySentReceiver.kt
+++ sms/src/main/java/com/beeper/sms/receivers/MySentReceiver.kt
@@ -9,6 +9,7 @@ import com.beeper.sms.CommandProcessor.Companion.EXTRA_COMMAND_ID
 import com.beeper.sms.Log
 import com.beeper.sms.commands.Command
 import com.beeper.sms.commands.incoming.SendMessage
+import com.beeper.sms.commands.outgoing.Error
 import com.beeper.sms.extensions.printExtras
 import com.beeper.sms.provider.SmsProvider
 import com.klinker.android.send_message.SentReceiver
@@ -22,7 +23,6 @@ class MySentReceiver : SentReceiver() {
             (intent?.getParcelableExtra(SENT_SMS_BUNDLE) as? Bundle)?.getInt(EXTRA_COMMAND_ID)
         val message = uri?.let { SmsProvider(context).getMessage(it) }
         when {
-            uri == null -> Log.e(TAG, "Missing uri")
             message != null ->
                 Bridge.INSTANCE.send(
                     Command(
@@ -31,7 +31,17 @@ class MySentReceiver : SentReceiver() {
                         commandId,
                     )
                 )
-            else -> Log.e(TAG, "Unable to respond (command=$commandId uri=$uri)")
+            commandId != null ->
+                Bridge.INSTANCE.send(
+                    commandId,
+                    Error(
+                        "missing_sms",
+                        "Message not found in Android's SMS database (uri=$uri)"
+                    )
+                )
+            else -> {
+                Log.e(TAG, "Unable to respond (command=$commandId uri=$uri)")
+            }
         }
     }
 
