095193dedb87281c36b610908984d839a60b23d5
Tiago Loureiro <tgloureiro@gmail.com>
2022-06-23 18:49:27 -0300

    Extract and add images and video sizes to retrieved mms attachments

--- sms/src/main/java/com/beeper/sms/provider/PartProvider.kt
+++ sms/src/main/java/com/beeper/sms/provider/PartProvider.kt
@@ -1,6 +1,8 @@
 package com.beeper.sms.provider
 
 import android.content.Context
+import android.graphics.BitmapFactory
+import android.media.MediaMetadataRetriever
 import android.provider.Telephony
 import android.provider.Telephony.Mms.Part.*
 import androidx.core.net.toUri
@@ -38,26 +40,78 @@ class PartProvider constructor(private val context: Context) {
                         folder.mkdir();
                     }
                     val fileNameSuffix = UUID.randomUUID().toString()
-                    val fileName = if(fileExtension!=null){
+                    val fileName = if (fileExtension != null) {
                         fileNameSuffix + ".${fileExtension}"
-                    }else{
+                    } else {
                         fileNameSuffix
                     }
-                    val file = File(context.mmsCache,  fileName)
+                    val file = File(context.mmsCache, fileName)
                     if (!file.exists()) {
                         file.createNewFile()
                     }
-                    cr.openInputStream("$URI_PART/$partId".toUri())?.writeTo(file)
+
+                    val uri = "$URI_PART/$partId".toUri()
+                    cr.openInputStream(uri)?.writeTo(file)
+
+                    val mediaThumbnailSize: Pair<Int, Int>? =
+                        extractMediaDimensions(mimetype, file)
+
                     Part(
                         attachment = Attachment(
                             mime_type = mimetype,
                             file_name = it.getString(NAME) ?: file.name,
                             path_on_disk = file.absolutePath,
+                            mediaThumbnailSize?.first,
+                            mediaThumbnailSize?.second
                         )
                     )
                 }
             }
         }
+
+    private fun extractMediaDimensions(
+        mimetype: String,
+        file: File
+    ): Pair<Int, Int>? {
+        try {
+            if (mimetype.startsWith("image")) {
+                    val options = BitmapFactory.Options()
+                    options.inJustDecodeBounds = true
+                    BitmapFactory.decodeStream(
+                        cr.openInputStream(file.toUri()),
+                        null,
+                        options
+                    )
+                    return Pair(options.outHeight, options.outWidth)
+            } else {
+                if (mimetype.startsWith("video")) {
+                        val mediaMetadataRetriever = MediaMetadataRetriever()
+                        val fd =
+                            cr.openFileDescriptor(file.toUri(), "r")?.fileDescriptor
+                        mediaMetadataRetriever.setDataSource(fd)
+                        val heightMetadata = mediaMetadataRetriever.extractMetadata(
+                            MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT
+                        )
+                        val widthMetadata = mediaMetadataRetriever.extractMetadata(
+                            MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH
+                        )
+                        if (heightMetadata != null && widthMetadata != null) {
+                            return Pair(
+                                Integer.valueOf(
+                                    heightMetadata
+                                ),
+                                Integer.valueOf(
+                                    widthMetadata
+                                ),
+                            )
+                        }
+                    }
+                }
+        } catch (e: Exception) {
+            // Ignore extracting metadata if something fails
+        }
+        return null
+    }
 
     private fun getMmsText(id: Long) =
         cr.openInputStream("$URI_PART/$id".toUri())?.use {
