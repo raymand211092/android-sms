e7a51001bf40d15016cfde26e257cb813956f35a
Tiago Loureiro <tgloureiro@gmail.com>
2022-07-08 21:51:21 +0000

    Notify when MMS fails

--- sms/build.gradle.kts
+++ sms/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 object Version {
     const val major = 0
     const val minor = 2
-    const val patch = 53
+    const val patch = 54
 }
 
 android {
@@ -109,7 +109,7 @@ afterEvaluate {
 
 dependencies {
     implementation("androidx.work:work-runtime-ktx:${Versions.work}")
-    implementation("com.gitlab.beeper:android-smsmms:4f1f6a73")
+    implementation("com.gitlab.beeper:android-smsmms:b70b8919")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.1")
     implementation("androidx.core:core-ktx:1.6.0")
     implementation("com.google.code.gson:gson:2.8.7")
