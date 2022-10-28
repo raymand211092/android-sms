da9422262cacdaf1fd6fabbdb48a8fd60449b6d4
Tiago Loureiro <tgloureiro@gmail.com>
2022-10-28 19:30:52 +0000

    Add address transform function to start new chat thread

--- sms/src/androidTest/java/com/beeper/sms/provider/GuidProviderTest.kt
+++ sms/src/androidTest/java/com/beeper/sms/provider/GuidProviderTest.kt
@@ -5,64 +5,64 @@ import org.junit.Test
 
 class GuidProviderTest {
     @Test
-    fun googleMountainView() = assertEquals("+16502530000", GuidProvider.normalizeAddress("(650) 253-0000"))
+    fun googleMountainView() = assertEquals("+16502530000", GuidProvider.transformToServerCompatibleAddress("(650) 253-0000"))
 
     @Test
-    fun googleChicago() = assertEquals("+13128404100", GuidProvider.normalizeAddress("+1 312-840-4100"))
+    fun googleChicago() = assertEquals("+13128404100", GuidProvider.transformToServerCompatibleAddress("+1 312-840-4100"))
 
     @Test
-    fun googleBerlin() = assertEquals("+4930303986300", GuidProvider.normalizeAddress("+49 30 303986300"))
+    fun googleBerlin() = assertEquals("+4930303986300", GuidProvider.transformToServerCompatibleAddress("+49 30 303986300"))
 
     @Test
-    fun googleBrazil() = assertEquals("+553121286800", GuidProvider.normalizeAddress("+55-31-2128-6800"))
+    fun googleBrazil() = assertEquals("+553121286800", GuidProvider.transformToServerCompatibleAddress("+55-31-2128-6800"))
 
     @Test
-    fun googleBangalore() = assertEquals("+918067218000", GuidProvider.normalizeAddress("+91-80-67218000"))
+    fun googleBangalore() = assertEquals("+918067218000", GuidProvider.transformToServerCompatibleAddress("+91-80-67218000"))
 
     @Test
-    fun googleDubai() = assertEquals("+97144509500", GuidProvider.normalizeAddress("+971 4 4509500"))
+    fun googleDubai() = assertEquals("+97144509500", GuidProvider.transformToServerCompatibleAddress("+971 4 4509500"))
 
     @Test
-    fun googleTelAviv() = assertEquals("+972747466453", GuidProvider.normalizeAddress("+972-74-746-6453"))
+    fun googleTelAviv() = assertEquals("+972747466453", GuidProvider.transformToServerCompatibleAddress("+972-74-746-6453"))
 
     @Test
     fun emailAddress() {
         // Address isn't necessarily a phone number, e.g. e-mail to SMS gateways
-        assertEquals("user@example.com", GuidProvider.normalizeAddress("user@example.com"))
+        assertEquals("user@example.com", GuidProvider.transformToServerCompatibleAddress("user@example.com"))
     }
 
     @Test
     fun emailAddressWithDisplayName() =
-        assertEquals("user@example.com", GuidProvider.normalizeAddress("User Name <user@example.com>"))
+        assertEquals("user@example.com", GuidProvider.transformToServerCompatibleAddress("User Name <user@example.com>"))
 
     @Test
     fun emailAddressWithQuotedDisplayName() =
-        assertEquals("user@example.com", GuidProvider.normalizeAddress("\"User Name\" <user@example.com>"))
+        assertEquals("user@example.com", GuidProvider.transformToServerCompatibleAddress("\"User Name\" <user@example.com>"))
 
     @Test
     fun emailAddressWithSubdomain() =
-        assertEquals("user@example.co.uk", GuidProvider.normalizeAddress("user@example.co.uk"))
+        assertEquals("user@example.co.uk", GuidProvider.transformToServerCompatibleAddress("user@example.co.uk"))
 
     @Test
     fun emailAddressWithoutAngleBrackets() =
-        assertEquals("user@example.com", GuidProvider.normalizeAddress("abc user@example.com 123"))
+        assertEquals("user@example.com", GuidProvider.transformToServerCompatibleAddress("abc user@example.com 123"))
 
     @Test
-    fun encodeSingleQuotations() = assertEquals("ab%27c", GuidProvider.normalizeAddress("ab'c"))
+    fun encodeSingleQuotations() = assertEquals("ab%27c", GuidProvider.transformToServerCompatibleAddress("ab'c"))
 
     @Test
-    fun encodeDoubleQuotations() = assertEquals("ab%22c", GuidProvider.normalizeAddress("ab\"c"))
+    fun encodeDoubleQuotations() = assertEquals("ab%22c", GuidProvider.transformToServerCompatibleAddress("ab\"c"))
 
     @Test
     //Input of this tests fails when starting a new chat if the dashes aren't removed
-    fun stripDashesIfNumber() = assertEquals("991976040", GuidProvider.normalizeAddress("9919-760-40"))
+    fun stripDashesIfNumber() = assertEquals("991976040", GuidProvider.transformToServerCompatibleAddress("9919-760-40"))
 
     @Test
-    fun encodeWhitespace() = assertEquals("wz%20data", GuidProvider.normalizeAddress("wz data"))
+    fun encodeWhitespace() = assertEquals("wz%20data", GuidProvider.transformToServerCompatibleAddress("wz data"))
 
     @Test
     fun decodeWhitespace() = assertEquals("wz data", GuidProvider.removeEscapingFromGuid("wz%20data"))
 
     @Test
-    fun preserveDashesIfNotANumber() = assertEquals("wz-data", GuidProvider.normalizeAddress("wz-data"))
+    fun preserveDashesIfNotANumber() = assertEquals("wz-data", GuidProvider.transformToServerCompatibleAddress("wz-data"))
 }
