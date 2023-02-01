8977cd2a9f1322b3a199a81d9be42eab3a19c55e
Tiago Loureiro <tgloureiro@gmail.com>
2023-02-01 15:16:56 +0000

    Fix duplicated inbound MMS

--- sms/build.gradle.kts
+++ sms/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 object Version {
     const val major = 0
     const val minor = 2
-    const val patch = 170
+    const val patch = 171
 }
 
 android {
@@ -109,7 +109,7 @@ afterEvaluate {
 
 dependencies {
     implementation("androidx.work:work-runtime-ktx:${Versions.work}")
-    implementation("com.gitlab.beeper:android-smsmms:0082d58c")
+    implementation("com.gitlab.beeper:android-smsmms:90401dfa")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.1")
     implementation("androidx.core:core-ktx:1.6.0")
     implementation("com.google.code.gson:gson:2.8.7")
